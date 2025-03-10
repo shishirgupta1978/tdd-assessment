73041335976d3056b00d7792793391188b406c41
Shishir Gupta <shishirgupta2011@gmail.com>
2025-03-10 18:49:48 +0530

    Handle newline delimiter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ class StringCalculator:
     def add(self, numbers):
         if numbers == "":
             return 0 # If the string is empty, return 0
+        numbers = numbers.replace("\n", ",") # Replace new lines with commas
         num_list = numbers.split(",")
         return sum([int(num) for num in num_list])
 
@@ -14,3 +15,4 @@ if __name__==   "__main__":
     print(calculator.add(""))
     print(calculator.add("1"))
     print(calculator.add("1,5"))
+    print(calculator.add("1\n2,3"))
--- test.py
+++ test.py
@@ -8,6 +8,23 @@ class TestStringCalculator(unittest.TestCase):
         result = calculator.add("")
         self.assertEqual(result, 0)
 
+    def test_single_number_string(self):
+        calculator = StringCalculator()
+        result = calculator.add("1")
+        self.assertEqual(result, 1)
+
+    def test_multiple_number_string(self):
+        calculator = StringCalculator()
+        result = calculator.add("1,5")
+        self.assertEqual(result, 6)
+
+    def test_newline_delimiter_string(self):
+        calculator = StringCalculator()
+        result = calculator.add("1\n2,3")
+        self.assertEqual(result, 6)
+
+
+
 
 
 if __name__ == "__main__":
